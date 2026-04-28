f315924643baf30742f4f7e05ff713910ac73a2f
Santanu-2002 <your-github-email@example.com>
2026-04-29 00:26:47 +0530

    secured

--- server/server.js
+++ server/server.js
@@ -1,14 +1,14 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
-
+require("dotenv").config();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
 // CONNECT DATABASE
-mongoose.connect("mongodb+srv://santanu_pinu:santanu2002@prototype.qb4dtaw.mongodb.net/myDB")
+mongoose.connect(process.env.MONGO_URI)
 .then(() => console.log("MongoDB connected"))
 .catch(err => console.log(err));
 
